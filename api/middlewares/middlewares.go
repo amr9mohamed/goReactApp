9b4e265b90b94859970876767fc345b00b30b0a1
Amr Mohamed <amr.mohamed@avelabs.com>
2021-09-11 22:09:21 +0200

    allow control allow orging for the client service

--- api/middlewares/middlewares.go
+++ api/middlewares/middlewares.go
@@ -5,6 +5,7 @@ import "net/http"
 func JsonMiddleware(hf http.HandlerFunc) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 		rw.Header().Set("Content-Type", "application/json")
+		rw.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 		hf(rw, r)
 	}
 }
