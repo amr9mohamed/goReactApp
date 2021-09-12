93f34b0c765d1aaf603bb7acbf5f7aaf60681cd7
Amr Mohamed <amr.mohamed@avelabs.com>
2021-09-12 21:20:10 +0200

    add freq comp to app

--- frontend/react_app/src/App.js
+++ frontend/react_app/src/App.js
@@ -1,27 +1,89 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Users } from "./components/Users";
+import { CountryFrequency } from "./components/CountryFrequency";
+import { makeStyles } from "@material-ui/core/styles";
+import InputLabel from "@material-ui/core/InputLabel";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import Button from "@material-ui/core/Button";
 import "./App.css";
 
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [users, setUsers] = useState([]);
   const [loading, setloading] = useState(false);
   const [usersPerPage] = useState(10);
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [states, setStates] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchCountries = async () => {
       setloading(true);
-      const res = await axios.get("http://localhost:8080/users");
-      setUsers(res.data);
+      const res = await axios.get("http://localhost:8080/users/countries");
+      setCountries(res.data);
       setloading(false);
     };
-    fetchUsers();
+    fetchCountries();
   }, []);
 
+  const handleChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const handleGetStates = () => {
+    axios
+      .get("http://localhost:8080/users/frequency")
+      .then((res) => {
+        res.data = res.data.map((el, idx) => {
+          return { ...el, id: idx };
+        });
+        setStates(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <div className="container mt-5">
+    <div>
       <h1 className="text-primary mb-3">Users</h1>
-      <Users users={users} loading={loading} usersPerPage={usersPerPage} />
+      <FormControl className={classes.formControl}>
+        <InputLabel htmlFor="country-native-simple">Country</InputLabel>
+        <Select
+          native
+          value={selectedCountry}
+          onChange={handleChange}
+          inputProps={{
+            name: "country",
+            id: "country-native-simple",
+          }}
+        >
+          <option aria-label="None" value="" />
+          {countries.map((country) => (
+            <option key={country} value={country}>
+              {country}
+            </option>
+          ))}
+        </Select>
+      </FormControl>
+      <br />
+      {users.length ? (
+        <Users users={users} loading={loading} usersPerPage={usersPerPage} />
+      ) : null}
+      <br />
+      <Button variant="contained" color="primary" onClick={handleGetStates}>
+        Show Stats
+      </Button>
+      {states.length ? (
+        <CountryFrequency states={states} loading={loading} />
+      ) : null}
     </div>
   );
 }
